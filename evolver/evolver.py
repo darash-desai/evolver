c063ef753fe7041d8c2670e1a83a3ea485d2134e
Darash Desai <darash@lyvewave.com>
2024-12-13 01:30:25 -0500

    Document evolver class

--- evolver/evolver.py
+++ evolver/evolver.py
@@ -8,33 +8,67 @@ class Evolver:
     """Singleton class for the eVOLVER control software."""
 
     _instance = None
-    _serial = None
-    _serialInput = ""
-    _readTimeout = 2
 
-    def __new__(singletonClass):
+    def __new__(singletonClass, serialPort="/dev/serial0", readTimeout=2) -> None:
         if singletonClass._instance is None:
-            singletonClass._instance = super().__new__(singletonClass)
+            logging.info("Creating eVOLVER singleton instance")
+            singletonClass._instance = super().__new__(
+                singletonClass, serialPort, readTimeout
+            )
+
         return singletonClass._instance
 
-    def __init__(self):
-        self._serial = serial.Serial("/dev/serial0", 9600)
-        self._serial.timeout = 5
-        logging.info("Creating eVOLVER singleton instance")
+    def __init__(self, serialPort: str, readTimeout: float) -> None:
+        """
+        Creates a new eVOLVER instances and connects to it.
 
-    def send(self, command):
+        Parameters:
+          serialPort (str): The serial port the eVOLVER is available on. Defaults to
+                            /dev/serial0.
+          readTimeout (float): The timeout for read operations in seconds.
+        """
+
+        self._serial = serial.Serial(serialPort, 9600)
+        self._readTimeout = readTimeout
+        self._serial.timeout = self._readTimeout
+        self._serialInput = ""
+
+    def send(self, command: str) -> str:
+        """
+        Sends a command the eVOLVER communication bus.
+
+        Parameters:
+          command (str): The command to send.
+
+        Returns:
+          str: The command response or an empty string if no response was received.
+        """
+
         logging.info(f"Sending eVOLVER command: {command}")
         self._serial.write(command.encode())
         self._serial.flush()
 
         response = self.__readResponse()
         logging.info(f"Received response: {response}")
+        return response
 
-    def disconnect(self):
+    def disconnect(self) -> None:
+        """
+        Disconnects from the eVOLVER system and closes the serial port.
+        """
+
         logging.info("Closing eVOLVER connection")
         self._serial.close()
 
-    def __readResponse(self):
+    def __readResponse(self) -> str:
+        """
+        Reads serial data from the eVOLVER until a complete response for a
+        command is received or the read timeout limit is met.
+
+        Returns:
+          str: A command response or an empty string if the read timed out.
+        """
+
         endTime = time() + self._readTimeout
         while time() < endTime:
             self._serialInput += self._serial.read_all().decode()
