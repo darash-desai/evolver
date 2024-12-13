694567f95fdbc2d609bcdd2ea93893b4d67adfd5
Darash Desai <darash@lyvewave.com>
2024-12-13 00:11:05 -0500

    Write command to serial

--- evolver/evolver.py
+++ evolver/evolver.py
@@ -15,10 +15,14 @@ class Evolver:
 
     def __init__(self):
         self._serial = serial.Serial("/dev/serial0", 9600)
+        self._serial.timeout = 5
         logging.info("Creating eVOLVER singleton instance")
 
     def send(self, command):
         logging.info(f"Sending eVOLVER command: {command}")
+        self._serial.write(command)
+        response = self._serial.read_until(b"end")
+        logging.info(f"Received response: {response}")
 
     def disconnect(self):
         logging.info("Closing eVOLVER connection")
