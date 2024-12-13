f2ae3f2e56c2fe2015398b064135385d0e2438cf
Darash Desai <55717275+darash-desai@users.noreply.github.com>
2024-12-13 01:45:39 -0500

    Implement basic communication with Arduino (#1)
    * Connect to serial device and set up command loop
    
    * Update output in send function
    
    * Update readme file
    
    * Write command to serial
    
    * Encode command written to serial output
    
    * Decode response from arduino
    
    * Flush serial
    
    * Implement a read until 'end' is received
    
    * Read response based on timeout instead of read attempts
    
    * Document evolver class
    
    * Fix __new__ error

--- evolver/evolver.py
+++ evolver/evolver.py
@@ -1,4 +1,7 @@
 import logging
+import serial
+
+from time import time
 
 
 class Evolver:
@@ -6,10 +9,71 @@ class Evolver:
 
     _instance = None
 
-    def __new__(singletonClass):
+    def __new__(singletonClass, *args, **kwargs) -> None:
         if singletonClass._instance is None:
-            singletonClass._instance = super().__new__(singletonClass)
+            logging.info("Creating eVOLVER singleton instance")
+            singletonClass._instance = super().__new__(singletonClass, *args, **kwargs)
+
         return singletonClass._instance
 
-    def __init__(self):
-        logging.info("Creating eVOLVER singleton instance")
+    def __init__(self, serialPort="/dev/serial0", readTimeout=2.0) -> None:
+        """
+        Creates a new eVOLVER instances and connects to it.
+
+        Parameters:
+          serialPort (str): The serial port the eVOLVER is available on. Defaults to
+                            /dev/serial0.
+          readTimeout (float): The timeout for read operations in seconds.
+        """
+
+        self._serial = serial.Serial(serialPort, 9600)
+        self._readTimeout = readTimeout
+        self._serial.timeout = self._readTimeout
+        self._serialInput = ""
+
+    def send(self, command: str) -> str:
+        """
+        Sends a command the eVOLVER communication bus.
+
+        Parameters:
+          command (str): The command to send.
+
+        Returns:
+          str: The command response or an empty string if no response was received.
+        """
+
+        logging.info(f"Sending eVOLVER command: {command}")
+        self._serial.write(command.encode())
+        self._serial.flush()
+
+        response = self.__readResponse()
+        logging.info(f"Received response: {response}")
+        return response
+
+    def disconnect(self) -> None:
+        """
+        Disconnects from the eVOLVER system and closes the serial port.
+        """
+
+        logging.info("Closing eVOLVER connection")
+        self._serial.close()
+
+    def __readResponse(self) -> str:
+        """
+        Reads serial data from the eVOLVER until a complete response for a
+        command is received or the read timeout limit is met.
+
+        Returns:
+          str: A command response or an empty string if the read timed out.
+        """
+
+        endTime = time() + self._readTimeout
+        while time() < endTime:
+            self._serialInput += self._serial.read_all().decode()
+            response, endSeq, remainder = self._serialInput.partition("end")
+            if endSeq != "":
+                self._serialInput = remainder
+                return f"{response}end"
+
+        logging.error(f"Received incomplete response: {self._serialInput}")
+        return ""
